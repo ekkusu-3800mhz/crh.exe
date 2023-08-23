5ddfcd1367efcd44ca6aa82edd376a77ac4030a4
X-Buster <shinra.dx@outlook.com>
2023-08-23 22:13:10 +0800

    新增中二节奏排队查询功能

--- src/plugins/queue.py
+++ src/plugins/queue.py
@@ -4,18 +4,21 @@ from nonebot import on_command, on_regex
 from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import Event, Bot, Message, MessageSegment
 
-from src.libraries.service import get_all, set_player
+from src.libraries.service import get_all_maimai, get_all_chunithm, set_player
 from src.libraries.msg_builder import build_msg
 
 
 # 获取所有机厅排队人数
 
-all_stats = on_command('all_stats ', aliases={'机厅几卡'})
+all_stats_maimai = on_command('all_stats_maimai ', aliases={'舞萌几卡'})
 
-@all_stats.handle()
+@all_stats_maimai.handle()
 async def _(bot: Bot, event: Event, state: T_State):
-    result = await get_all()
+    result = await get_all_maimai()
     msg = []
+    msg.append(MessageSegment("text", {
+        "text": f"【全域舞萌DX排队情况】"
+    }))
     if not result['data']['error']:
         for info in result['data']['cabinetList']:
             msg.append(MessageSegment("text", {
@@ -25,17 +28,38 @@ async def _(bot: Bot, event: Event, state: T_State):
         msg.append(MessageSegment("text", {
             "text": result['data']['error'],
         }))
-    await all_stats.send(Message(msg))
+    await all_stats_maimai.send(Message(msg))
+
+
+all_stats_chunithm = on_command('all_stats_chunithm ', aliases={'中二几卡'})
+
+@all_stats_chunithm.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    result = await get_all_chunithm()
+    msg = []
+    msg.append(MessageSegment("text", {
+        "text": f"【全域中二节奏NEW排队情况】"
+    }))
+    if not result['data']['error']:
+        for info in result['data']['cabinetList']:
+            msg.append(MessageSegment("text", {
+                "text": build_msg(info),
+            }))
+    else:
+        msg.append(MessageSegment("text", {
+            "text": result['data']['error'],
+        }))
+    await all_stats_chunithm.send(Message(msg))
 
 
 # 设置指定机台的排队人数
 
-player_cal = on_regex(r"^(.+)(\d+)卡$")
+player_cal = on_regex(r"^([\u4e00-\u9fa5]+)(\d+)卡$")
 
 @player_cal.handle()
 async def _(bot: Bot, event: Event, state: T_State):
     # 解构QQ消息正文，提取机台名称和排队人数
-    regex = "^(.+)(\d+)卡$"
+    regex = "^([\u4e00-\u9fa5]+)(\d+)卡$"
     res = re.match(regex, str(event.get_message()).lower()).groups()
     command = res[0]
     number = int(res[1])
